0a9a51c466a90f1081be703bc6890c37168adc42
Shadha Zakharia <shradhazakharia3323@gmail.com>
2024-04-08 17:00:30 +0100

    Answering the part two question

--- Debugging and Testing.py
+++ Debugging and Testing.py
@@ -38,3 +38,33 @@ def red_or_blue(num):
     if within_range(num, 2, 5) and num > 20:
         return 'Blue'
 
+
+### TASK TWO -- TDD ###
+
+"""
+Let's write a test for our function first and then will write an actual code to ensure that
+all tests pass. 
+
+Task
+Given a list of dictionaries where keys are student's  name and student's mark.
+calculate the average score for the exam.
+
+If mark is not within the range 1 to 10, raise an error
+Some mark values can be integers and some are strings, we need to process both
+If mark is missing, use value 5
+
+"""
+from unittest import TestCase, main
+class TestAverageExamScore(TestCase):
+
+    def test_calculate_average(self):
+        my_input = [
+            {'name': 'Jane', 'Marks': 7},
+            {'name': 'Nitesh', 'Marks': 6},
+            {'name': 'Aisha', 'Marks': 8},
+            {'name': 'Zac', 'Marks': 8},
+        ]
+
+        expected = 7.25
+
+        result
